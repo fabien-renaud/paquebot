5a105328af113d4223f175826e0f28e5d1e023a6
Fabien RENAUD <frenaud@hipay.com>
2021-10-14 13:54:26 +0200

    Fix import

--- src/core/discord.js
+++ src/core/discord.js
@@ -1,7 +1,7 @@
 import {Client, Intents} from 'discord.js';
 import {SlashCommandBuilder} from '@discordjs/builders';
 import {REST} from '@discordjs/rest';
-import {Routes} from 'discord-api-types';
+import {Routes} from 'discord-api-types/v9';
 
 const {CLIENT_ID: clientId, GUILD_ID: guildId, DISCORD_TOKEN: token} = process.env;
 
